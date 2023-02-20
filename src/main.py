6dd8ac7eec02eeb56e3de9ba936b7b855a69d964
10-15-5 <hpjpm@protonmail.com>
2023-02-20 17:14:03 +0000

    Playlists created with track ids from m3u file

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 import configparser
 import pathlib
 
+from datetime import datetime
 from spotipy.oauth2 import SpotifyOAuth
 from m3u_parser import M3uParser
 
@@ -45,14 +46,11 @@ def main():
 
     for i in file_paths:
         tracks = get_tracks_from_playlist(i)
-        # update_playlist(tracks)
-        # update_playlist()
-
-        # print(pathlib.Path().resolve())
-        print(config.get("CONFIG", "CLIENT_ID"))
+        playlist_name = i.split("\\")[-1].split(".")[0]
+        spotify_interaction(tracks, playlist_name)
 
 def get_tracks_from_playlist(path_to_file):
-    print(path_to_file)
+    tracks = []
     useragent = config.get("CONFIG", "USER_AGENT")
     parser = M3uParser(timeout=5, useragent=useragent)
     parser.parse_m3u(path_to_file)
@@ -60,7 +58,48 @@ def get_tracks_from_playlist(path_to_file):
     playlist_list = parser.get_list()
 
     for i in range(len(playlist_list)):
-        print(playlist_list[i]["name"])
+        tracks.append(playlist_list[i]["name"])
+
+    return tracks
+
+
+def spotify_interaction(playlist_tracks, playlist_name):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config.get("CONFIG", "CLIENT_ID"),
+                                                client_secret=config.get("CONFIG", "CLIENT_SECRET"),
+                                                redirect_uri="http://localhost/",
+                                                scope="playlist-modify-public"))
+    
+    playlist_id = create_playlist(sp, playlist_name)
+
+    playlist_track_ids = get_track_ids(sp, playlist_tracks)
+
+    update_playlist(sp, playlist_track_ids, playlist_id)
+    
+
+def create_playlist(sp, playlist_name):
+    playlist_id = sp.user_playlist_create(sp.current_user()["id"], 
+                            playlist_name, 
+                            public=True, 
+                            collaborative=False, 
+                            description="Auto-generated playlist created on " + str(datetime.now())
+                            )["id"]
+    
+    return playlist_id
+
+
+def get_track_ids(sp, playlist_tracks):
+
+    for i in range(len(playlist_tracks)):
+        track_url = sp.search(playlist_tracks[i],limit=1,type="track",market=config.get("CONFIG", "COUNTRY_CODE"))["tracks"]["items"][0]["external_urls"]["spotify"]
+        track_id = track_url.split("/")[-1]
+        
+        playlist_tracks[i] = track_id
+
+    return playlist_tracks
+
+def update_playlist(sp, tracks, id):
+
+    sp.user_playlist_replace_tracks(sp.current_user()["id"], id, tracks)
 
 
 if __name__ == "__main__":
